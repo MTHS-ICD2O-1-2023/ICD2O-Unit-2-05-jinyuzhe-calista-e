9e629ea86faca6f04ad3c140cc1c54b4832833e1
Calista.E <159185314+jinyuzhe-calista-e@users.noreply.github.com>
2024-03-25 18:41:26 +0000

    done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Calista.E
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates area of a triangle.
+ */
+function calculatePayAndTaxesOfSalary () {
+  const TAX_RATE = 0.18
+  // input
+  const workHours = parseInt(document.getElementById('work-hours').value)
+  const hourlyRate = parseInt(document.getElementById('hourly-rate').value)
+
+  // process
+  const payOfSalary = workHours * hourlyRate * (1.00 - TAX_RATE)
+  const taxesOfSalary = workHours * hourlyRate * TAX_RATE
+
+  // output
+  document.getElementById('pay').innerHTML = 'Pay is: $' + payOfSalary.toFixed(2)
+  document.getElementById('taxes').innerHTML = 'Taxes is: $' + taxesOfSalary.toFixed(2)
+
+}
